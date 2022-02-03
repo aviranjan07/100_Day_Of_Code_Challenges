3b52986a02e4d4225594f3d03995b4a80e7c80fd
aviranjan07 <ad3447578@gmail.com>
2022-02-03 14:10:16 +0530

    Project: degital clock by youtube

---
+++ day84/degitalClock/app.js
@@ -0,0 +1,31 @@
+function showTime(){ 
+    var date = new Date();
+    var h  = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+
+  if(h==0){
+      h = 12;
+  }
+
+  if(h>12){
+      h=h-12;
+      session = "PM"
+  }
+    //two degit formet
+  if(h<10){
+      h="0"+h;
+  }
+  if(m<10){
+      m="0"+m;
+  }
+  if(s<10){
+      s="0"+s;
+  }
+    
+document.getElementById("DegitalClock").innerHTML = h + ":" + m + ":" + s + "" + session; 
+
+setTimeout(showTime,1000);
+} 
+showTime();  
