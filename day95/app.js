178dc38a57b0f1081553a1c8d2b9723eb5ef7a7e
aviranjan07 <ad3447578@gmail.com>
2022-02-14 11:41:00 +0530

    update

--- day95/app.js
+++ day95/app.js
@@ -79,7 +79,7 @@ function getRemaindingTime() {
 
   if (t < 0) {
     clearInterval(countdown);
-    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    deadline.innerHTML = `<h4 class="expired">sorry, this soshthi has expired!</h4>`;
   }
 }
 // countdown;
